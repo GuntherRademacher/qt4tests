7784f204145286b45c164b8b9777eadb800f4253
Gunther Rademacher <grd@gmx.net>
2023-11-17 15:48:53 +0100

    trying to regenerate fn:parse-html test results

--- misc/HtmlTestSuite/tools/generateXhtml/src/Main.java
+++ misc/HtmlTestSuite/tools/generateXhtml/src/Main.java
@@ -1,30 +1,39 @@
+import java.io.File;
+import java.io.PrintStream;
+import java.io.StringWriter;
+import java.nio.file.Files;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
 import net.sf.saxon.s9api.Processor;
 import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.Serializer;
 import net.sf.saxon.s9api.XdmNode;
-import org.jsoup.nodes.Document;
-import org.jsoup.parser.Parser;
-
-import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         Parser parser = Parser.htmlParser();
 
-        File dir = new File("/Users/mike/GitHub/qt4cg/qt4tests/misc/HtmlTestSuite/files/");
-        File outDir = new File("/Users/mike/GitHub/qt4cg/qt4tests/misc/HtmlTestSuite/results/");
+        File dir = new File("../../files/");
+        File outDir = new File("../../results/");
         File[] files = dir.listFiles();
-        
+
         for (File f : files) {
             try {
                 System.err.println("Processing " + f.getName());
-                Document doc = parser.parseInput(new BufferedReader(new FileReader(f)), dir.toURI().toString());
+                // replace Windows line separators
+                String html = Files.readString(f.toPath()).replace("\r\n", "\n");
+                Document doc = parser.parseInput(html, dir.toURI().toString());
                 String xml = serialize(doc);
                 File outFile = new File(outDir, f.getName().replace(".html", ".xhtml"));
                 outFile.createNewFile();
-                PrintStream ps = new PrintStream(outFile);
-                ps.print(xml);
+                try (PrintStream ps = new PrintStream(outFile)) {
+                    // install system line separators
+                    xml = xml.replace("\n", System.lineSeparator());
+                    ps.print(xml);
+                }
             } catch (Exception e) {
                 System.err.println("**** Failed to convert " + f.getName() + " - " + e.getMessage());
             }
